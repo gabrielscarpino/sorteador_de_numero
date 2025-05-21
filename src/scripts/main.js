89681bf9a65558ddd617131511fc449e0a815003
gabriel <gabrielscarpino@hotmail.com>
2025-05-21 16:38:18 -0300

    correção de escrita no arquivo main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
-Document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('form-sorteador').addEventListener('submit', function(evento){
         evento.preventDefault();
         let numeroMaximo = document.getElementById('numero-maximo').value;
-        numeroMaximo = parseint(numeroMaximo);
+        numeroMaximo = parseInt(numeroMaximo);
 
-        let numeroAleatorio = Math.randow() * numeroMaximo;
+        let numeroAleatorio = Math.random() * numeroMaximo;
         numeroAleatorio = Math.floor(numeroAleatorio + 1);
         document.getElementById('resultado-valor').innerText = numeroAleatorio;
         document.querySelector('.resultado').style.display = 'block';
